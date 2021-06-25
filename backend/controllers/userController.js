e00b3bf6e6ad46011df91e515579444ef2a71f28
phong764119 <phong764119@gmail.com>
2021-06-25 16:15:17 +0700

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -15,39 +15,47 @@ const authUser = asyncHandler(async (req, res) => {
   if (user && (await user.matchPassword(password))) {
     var obj = {
       _id: user._id,
-      user_level: user.user_level,
+      role: user.role,
     };
     res.json({
       id: user._id,
-      user_level: user.user_level,
-      username: username,
-      full_name: user.full_name,
+      role: user.role,
+      username: user.username,
+      email: user.email,
+      fullName: user.fullName,
+      phone: user.phone,
+      points: user.points,
+      confirmed: user.confirmed,
       token: generateToken(obj),
     });
   } else {
     res.status(401);
-    throw new Error("Invalid email or password");
+    throw new Error("invalid.login");
   }
 });
 
 const registerUser = asyncHandler(async (req, res) => {
-  const { username, password, user_level, full_name } = req.body;
+  const { username, password, email, fullName, phone } = req.body;
   const userExists = await User.findOne({ username }).exec();
   if (userExists) {
     res.status(400);
-    throw new Error("User already exists");
+    throw new Error("user.existing");
   }
 
-  const user = await User.create({ username, password, user_level, full_name });
+  const user = await User.create({ username, password, email, fullName, phone });
   var obj = {
     _id: user._id,
-    user_level: user.user_level,
+    role: user.role,
   };
   res.json({
     id: user._id,
-    user_level: user.user_level,
-    username: username,
-    full_name: user.full_name,
+    role: user.role,
+    username: user.username,
+    email: user.email,
+    fullName: user.fullName,
+    phone: user.phone,
+    points: user.points,
+    confirmed: user.confirmed,
     token: generateToken(obj),
   });
 });
@@ -55,21 +63,21 @@ const registerUser = asyncHandler(async (req, res) => {
 const updateUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id).exec();
   if (user) {
-    user.full_name = req.body.full_name || user.full_name;
+    user.fullName = req.body.fullName || user.fullName;
 
-    if (req.body.username) {
-      var e = await User.findOne({ username: req.body.username }).exec();
+    if (req.body.email) {
+      var e = await User.findOne({ username: req.body.email }).exec();
       if (e) {
         res.status(400);
-        throw new Error("Email is already exist");
-      } else user.username = req.body.username;
+        throw new Error("email.existing");
+      } else user.email = req.body.email;
     }
 
     if (req.body.password) {
       if (await user.matchPassword(req.body.oldPassword)) user.password = req.body.password;
       else {
         res.status(400);
-        throw new Error("Wrong password");
+        throw new Error("wrong.password");
       }
     }
     await user.save();
